875e70f97910d1f6f15e51a0d29adeea1a74d83a
Евгений Юрьевич <anikjevjenya@gmail.com>
2023-12-10 22:14:39 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <windows.h>
+
+void PrintScript(const char text[]) {
+
+	for (int i = 0; text[i] != '\0'; i++)
+	{
+		printf("%c", text[i]);
+		Sleep(100);
+	}
+
+}
+
 
 void main() 
 {
 	system("chcp 1251");
 
-	int a;
 
-	while (true) {				
-		printf("это а\n"); 
-		system("pause > null");
-	}
+	PrintScript("Иди нахуй сука\n");
+
+	system("pause > null");
 }
